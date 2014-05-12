32db3a3e96ab3981e7869eca104bd71da004237d
Lubomír Štěpánek <lubomir.stepanek@gmail.com>
2014-05-12 03:09:27 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+# a working directory is set and data are loaded into "data" dataframe
 setwd("C:/Users/Lubomír Štěpánek/Documents/Data Science Specialization/Exploratory Data Analysis/Week 1")
 
 data<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
@@ -6,10 +7,12 @@ str(data)
 
 which(data$Date=="1/2/2007"|data$Date=="2/2/2007")
 
+# new data called "data2" are extracted from the original set "data" and indicator variable of time of the two days (from 1 to length of data2) is added
 data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
 data2<-data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
 data2<-cbind(data2,c(1:dim(data2)[1]))
 
+# the plot is created
 png("plot2.png",units="px",width=480,height=480)
 plot(data2[,10],data2$Global_active_power,type="s",xaxt="n",xlab="",main="",ylab="Global active power (kilowatts)")
 axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
