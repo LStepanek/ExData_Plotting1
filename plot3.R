1ac801b697895e651a092b5c0391eb2092a40671
Lubomír Štěpánek <lubomir.stepanek@gmail.com>
2014-05-12 02:38:30 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,17 @@
+setwd("C:/Users/Lubomír Štěpánek/Documents/Data Science Specialization/Exploratory Data Analysis/Week 1")
+
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+
+str(data)
+
+data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
+data2<-data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
+data2<-cbind(data2,c(1:dim(data2)[1]))
+
+png("plot3.png",units="px",width=480,height=480)
+plot(data2[,10],data2$Sub_metering_1,type="s",xaxt="n",xlab="",ylab="energy sub metering")
+points(data2$Sub_metering_2~data2[,10],col="red",type="s")
+points(data2$Sub_metering_3~data2[,10],col="blue",type="s")
+axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),fill=c("black","red","blue"))
+dev.off()
